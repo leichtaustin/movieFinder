1bdd978f0c10a950085d13730e884ab569a1b095
Austin Leicht <leichtaustin@gmail.com>
2023-08-09 21:09:58 -0600

    Updated load data function

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { clearSearchResults, selectSearchResults, updateSearchResults } from "./searchResultsSlice";
+import { selectSearchResults, updateSearchResults } from "./searchResultsSlice";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { fetchNextPage } from "../api";
 import { selectSearchTerm } from "../SearchBar/searchTermSlice";
+import './searchResults.css';
 
 export const SearchResults = () => {
     const results = useSelector(selectSearchResults);
@@ -18,7 +19,7 @@ export const SearchResults = () => {
         } catch (rejectedValueOrSerializedError) {
             throw new Error(rejectedValueOrSerializedError);
         }
-        dispatch(clearSearchResults());
+        //dispatch(clearSearchResults());
         nextPageResults.Search.map(element => {
             dispatch(updateSearchResults(element));
             return element;
@@ -26,34 +27,15 @@ export const SearchResults = () => {
         setPageNumber(pageNumber => pageNumber + 1);
 
     }
-
-    const getPrevPage = async () => {
-        if(pageNumber === 1) {
-            return;
-        }
-        try {
-            nextPageResults = await fetchNextPage(searchTerm, pageNumber - 1);
-        } catch (rejectedValueOrSerializedError) {
-            throw new Error(rejectedValueOrSerializedError);
-        }
-        dispatch(clearSearchResults());
-        nextPageResults.Search.map(element => {
-            dispatch(updateSearchResults(element));
-            return element;
-        })
-        setPageNumber(pageNumber => pageNumber - 1);
-
-    }
-    
+   
     return (
-        <div>
+        <div className="searchResultsContainer">
             {results.map(movie => {
                 return (
                     <MovieCard movie = {movie} />
                 )
             })}
-            {(pageNumber !== 1) ? (<button onClick={getPrevPage}>PrevPage</button>) : null}
-            {(results.length === 0) ? null : (<button onClick={getNextPage}>NextPage</button>)}
+            {(results.length === 0) ? null : (<button onMouseOver={getNextPage}>NextPage</button>)}
         </div>
     )
 }
